3cdc0657bd8ca40def44ca7556ff2cc58162f99e
Vishal Bhardwaj <vishalbinnojs@gmail.com>
2026-02-05 21:25:30 +0530

    file formatted

--- Color_Switcher/src/App.jsx
+++ Color_Switcher/src/App.jsx
@@ -1,27 +1,37 @@
-import {useState} from "react";
+import { useState } from "react";
 import "./App.css";
-import Button from "./components/Button"
-import ColorBox from "./components/ColorBox"
+import Button from "./components/Button";
+import ColorBox from "./components/ColorBox";
 const App = () => {
-    const colors = ["pink",'red','blue','green','yellow','orange','brown','lightseagreen',"transparent"];
+  const colors = [
+    "pink",
+    "red",
+    "blue",
+    "green",
+    "yellow",
+    "orange",
+    "brown",
+    "lightseagreen",
+    "transparent",
+  ];
 
-const [boxColor,setBoxColor] = useState("")
+  const [boxColor, setBoxColor] = useState("");
   return (
     <>
-    <h1 className=' text-center text-3xl my-10'>🌈 Color Switcher</h1>
+      <h1 className=" text-center text-3xl my-10">🌈 Color Switcher</h1>
 
-    <div className="flex justify-center gap-4">
-        {
-            colors.map((color,index)=>(
-                <Button key={index} color={color} setBoxColor={setBoxColor}>{color}</Button>
-// Here {color} passed to the <Button></Button> component is directly accessed using children property in the button component.
-            ))
-        }
-    </div>
+      <div className="flex justify-center gap-4">
+        {colors.map((color, index) => (
+          <Button key={index} color={color} setBoxColor={setBoxColor}>
+            {color}
+          </Button>
+          // Here {color} passed to the <Button></Button> component is directly accessed using children property in the button component.
+        ))}
+      </div>
 
-    <ColorBox boxColor={boxColor}/>
+      <ColorBox boxColor={boxColor} />
     </>
-  )
+  );
 };
 
 export default App;
