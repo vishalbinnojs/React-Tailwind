9203faa6171c6e9d29d12f84395b0ded16ceee49
Vishal Bhardwaj <vishalbinnojs@gmail.com>
2026-07-10 08:44:12 +0530

    comment update

--- Fetch_Axios/src/components/Fetch.jsx
+++ Fetch_Axios/src/components/Fetch.jsx
@@ -26,6 +26,11 @@ const Fetch = () => {
         console.error("Failed to fetch data....! ", error);
       }
     };
+    
+    // const fetchData = ()=>fetch("https://jsonplaceholder.typicode.com/users")
+    // .then(res => res.json())
+    // .then(data => setUsers(data));
+
     fetchData();
   }, []);
   const addDetails = async () => {
